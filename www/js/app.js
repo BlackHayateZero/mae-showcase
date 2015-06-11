e8ee7c46f2272d68edfe629e80ef3cee1d728601
Ignatius Seto <ignatius.seto@zalando.de>
2015-06-11 21:16:35 +0200

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,70 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+    /* default
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })*/
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleLightContent();
+            }
+        });
+    })
+
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+
+            .state('tab', {
+                url: "/tab",
+                abstract: true,
+                templateUrl: "templates/tabs.html"
+            })
+
+            .state('tab.home', {
+                url: '/home',
+                views: {
+                    'tab-home': {
+                        templateUrl: 'templates/tab-home.html',
+                        controller: 'HomeCtrl'
+                    }
+                }
+            })
+
+            .state('tab.themen', {
+                url: '/themen',
+                views: {
+                    'tab-themen': {
+                        templateUrl: 'templates/tab-themen.html',
+                        controller: 'ThemenCtrl'
+                    }
+                }
+            })
+
+        $urlRouterProvider.otherwise('/tab/home');
+    });
---
+++ www/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('starter.controllers', [])
+
+    /* Controller for home*/
+    .controller('HomeCtrl', function ($scope) {})
+
+    /* Controller for Themen*/
+    .controller('ThemenCtrl', function ($scope) {})
